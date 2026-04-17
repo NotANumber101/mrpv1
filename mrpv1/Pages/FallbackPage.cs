c7e92c327f51b27722b05776c06c3427dfa5ed6e
sergei <liddell.s@pm.me>
2026-04-17 09:00:35 -0700

    moves db checker to fallback page

--- mrpv1/Pages/FallbackPage.cs
+++ mrpv1/Pages/FallbackPage.cs
@@ -13,6 +13,24 @@ public class FallbackPage() : Page
 {
     public async Task Display()
     {
+
+        try
+        {
+            var dbsb = new DbSourceBuilder("db,localhost");
+            await using var dataSource = dbsb.Builder().BuildMultiHost();
+            // FETCH DATA
+            await using var connection = await dataSource.OpenConnectionAsync();
+            // SELECT 8 verifies Logging
+            await using var loggingCommand = new NpgsqlCommand("SELECT 8", connection);
+            _ = await loggingCommand.ExecuteScalarAsync();
+            AnsiConsole.MarkupLine("LOG: Database Connection: [green]OK![/]");
+        }
+        catch (NpgsqlException e)
+        {
+            AnsiConsole.MarkupLine("[red]WARN: Unable to retrieve data...[/]");
+            AnsiConsole.MarkupLine("[red]ERROR: Database server connection has failed.[/]");
+            Console.WriteLine(e.Message);
+        }
         DbMetaController dbMetaController = new DbMetaController();
         List<string> dbTableNames = await dbMetaController.GetDbTableNames();
         List<string> dbFieldNames = await dbMetaController.GetTableFieldNames();
--- mrpv1/Program.cs
+++ mrpv1/Program.cs
@@ -4,23 +4,6 @@ using mrpv1.Helpers;
 using mrpv1.Pages;
 using Npgsql;
 
-try
-{
-    var dbsb = new DbSourceBuilder("db,localhost");
-    await using var dataSource = dbsb.Builder().BuildMultiHost();
-    // FETCH DATA
-    await using var connection = await dataSource.OpenConnectionAsync();
-    // SELECT 8 verifies Logging
-    await using var loggingCommand = new NpgsqlCommand("SELECT 8", connection);
-    _ = await loggingCommand.ExecuteScalarAsync();
-    AnsiConsole.MarkupLine("LOG: Database Connection: [green]OK![/]");
-}
-catch (NpgsqlException e)
-{
-    AnsiConsole.MarkupLine("[red]WARN: Unable to retrieve data...[/]");
-    AnsiConsole.MarkupLine("[red]ERROR: Database server connection has failed.[/]");
-    Console.WriteLine(e.Message);
-}
 if (AnsiConsole.Profile.Capabilities.Interactive)
 {
     // AnsiConsole.Clear();
